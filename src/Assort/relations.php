d62ea58ab8e02e1df2e5802145a2468e52539063
hadi <hadi@dpq.co.ir>
2017-06-10 16:38:50 +0430

    initial structure and first version

---
+++ src/Assort/relations.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'SDP_Category' => array(
+    // XXX: note: hadi, 1396-03: commented to avoid casecade deleting *****
+//         'relate_to' => array(
+//             'CMS_Content',
+//             'SDP_Category'
+//         ),
+    // *****
+    )    
+);
