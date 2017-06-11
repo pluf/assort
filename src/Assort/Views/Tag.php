2534e95fe83374e7fe5bbe1bd19a4872468b6300
hadi <hadi@dpq.co.ir>
2017-06-12 02:12:59 +0430

    edit a method

--- src/Assort/Views/Tag.php
+++ src/Assort/Views/Tag.php
@@ -5,9 +5,25 @@ Pluf::loadFunction('Assort_Shortcuts_GetTagByNameOr404');
 class Assort_Views_Tag
 {
 
-    public static function getByName($request, $match)
+    /**
+     * Returns tag with given name.
+     *
+     * @param Pluf_HTTP_Request $request
+     * @param array $match
+     * @return Assort_Tag
+     */
+    public static function getTagByName($request, $match)
     {
         $tag = Assort_Shortcuts_GetTagByNameOr404($match['name']);
+        // حق دسترسی
+        // CMS_Precondition::userCanAccessContent($request, $content);
+        // اجرای درخواست
+        return $tag;
+    }
+    
+    public static function getByName($request, $match)
+    {
+        $tag = Assort_Views_Tag::getTagByName($request, $match);
         // حق دسترسی
         // CMS_Precondition::userCanAccessContent($request, $content);
         // اجرای درخواست
